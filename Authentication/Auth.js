3a0b24febef73d676eebd7c9c8481069d41c62b0
VENKATTARAMANAN <ramanan451@gmail.com>
2023-10-06 23:01:32 +0530

    vercel eighteen

--- Authentication/Auth.js
+++ Authentication/Auth.js
@@ -2,7 +2,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import jwt from "jsonwebtoken"
 export async function isAuthenticated(req,res,next){
-    const token=req.headers.Authorization;
+    console.log(req.headers.authorization);
+    const token=req.headers.authorization;
+    console.log('token::: ', token);
     if(!token){
         return res.status(400).json({data:"Invalid Authorization"})
     }
